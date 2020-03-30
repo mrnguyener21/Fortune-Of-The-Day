ffff6cdf4381b80959358eaf453c842a5b143a1a
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-03-29 22:52:44 -0700

    began basic outline and brainstorming

---
+++ src/component/Fortune.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Fortunes extends React.Component{
+    state={
+        fortunes: null,
+    }
+
+    fortuneOptions = {
+        fortune1: 'the grass is greener on the other side',
+        fortune2: 'there is always light at the end of the tunnel',
+        fortune3: 'sometimes the person we are looking for the most is ourself'
+    }
+
+    
+
+}
