af104577d1708b49520354707bf208027429ebe2
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-03-31 22:40:10 -0700

    succesfully connected components

--- src/App.js
+++ src/App.js
@@ -1,2 +1,14 @@
 import React from 'react';
-import Fortune from './component/Fortune';
+import Fortunes from './component/Fortune.js'
+
+const App = () => {
+    return(
+    <div>
+        <h1>APP</h1>
+        <Fortunes/>
+    </div>
+
+    )
+}
+
+export default App;
--- src/component/Fortune.js
+++ src/component/Fortune.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 class Fortunes extends React.Component{
-    state={
-        fortunes: null,
-    }
 
-    fortuneOptions = {
-        fortune1: 'the grass is greener on the other side',
-        fortune2: 'there is always light at the end of the tunnel',
-        fortune3: 'sometimes the person we are looking for the most is ourself'
-    }
+    // state={
+    //     fortunes: null,
+    // }
 
-    
+    // fortuneOptions = {
+    //     fortune1: 'the grass is greener on the other side',
+    //     fortune2: 'there is always light at the end of the tunnel',
+    //     fortune3: 'sometimes the person we are looking for the most is ourself'
+    // }
+    render(){
+        return(
+            <div><h1>HELLO</h1></div>   
+        )
+    }
+    //gonna need to make an if else statement to determine if fortune is 1,2, or 3 and from there will change the setState to the corresponding fortune
+    //create a math.random for as the callbackfunction after the button gets clicked 
 
 }
+export default Fortunes;
