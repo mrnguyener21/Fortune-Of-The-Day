23ac31c5251e019285b73aa99fc0ff7f4f79c24c
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-04-01 07:35:13 -0700

    created a button to cycle between a range of 1-3

--- src/component/Fortune.js
+++ src/component/Fortune.js
@@ -1,19 +1,27 @@
 import React from 'react';
 
 class Fortunes extends React.Component{
+    state={
+        number: 0,
+        fortunes: null,
+    };
 
-    // state={
-    //     fortunes: null,
-    // }
-
-    // fortuneOptions = {
-    //     fortune1: 'the grass is greener on the other side',
-    //     fortune2: 'there is always light at the end of the tunnel',
-    //     fortune3: 'sometimes the person we are looking for the most is ourself'
-    // }
+    fortuneOptions = {
+        fortune1: 'the grass is greener on the other side',
+        fortune2: 'there is always light at the end of the tunnel',
+        fortune3: 'sometimes the person we are looking for the most is ourself'
+    };
+    number = () => this.setState({number:Math.floor(Math.random()*3+1)}); 
+    // increment = this.setState({number:this.state.number +1});
+    
+    
     render(){
         return(
-            <div><h1>HELLO</h1></div>   
+            <>
+                <button onClick={this.number}>CLICK</button>   
+                <h1>{this.state.number}</h1>
+                <h1>{this.state.fortunes}</h1>
+            </>
         )
     }
     //gonna need to make an if else statement to determine if fortune is 1,2, or 3 and from there will change the setState to the corresponding fortune
