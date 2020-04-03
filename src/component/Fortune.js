c3764d3162cb8a84e469f9fe9905746207f2c5f0
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-04-02 21:11:23 -0700

    fixed issue where fortune was matching the previous number

--- src/component/Fortune.js
+++ src/component/Fortune.js
@@ -6,34 +6,36 @@ class Fortunes extends React.Component{
         number: null,
         fortunes: null,
     };
-
     fortuneOptions = {
         fortune1: 'the grass is greener on the other side',
         fortune2: 'there is always light at the end of the tunnel',
-        fortune3: 'sometimes the person we are looking for the most is ourself'
+        fortune3: 'sometimes the person we are looking for the most is ourself',
     };
-    number = () => {
+
+    stateDetail = () => {
         this.setState({number:Math.floor(Math.random()*3+1)});
         
-        const fortune = () => {
+        //why is my if/else statement reflecting the previous number
+        //ANSWER BECAUSE WHEN I CLICK BOTH NUMBER AND FORTUNE GET READ AT THE SAME TIME, THE CHANGE TO FORTUNE HAS TO HAPPEN AFTER NUMBER IS CHANGED
+        setTimeout(()=>{
             if(this.state.number === 1){
-                this.setState({fortunes: this.fortuneOptions.fortune1});
+                this.setState({fortunes: this.fortuneOptions.fortune1});//the grass is greener on the other side',
             }else if(this.state.number === 2){
-                this.setState({fortunes: this.fortuneOptions.fortune2});
+                this.setState({fortunes: this.fortuneOptions.fortune2});//'there is always light at the end of the tunnel',
             }else if(this.state.number === 3){
-                this.setState({fortunes:this.fortuneOptions.fortune3});
-            }else{
-                this.setState({fortunes:null});
+                this.setState({fortunes:this.fortuneOptions.fortune3});//'sometimes the person we are looking for the most is ourself',
             };
-        };
-        fortune();
+        },10)
+        
     }
-
+    
     render(){
         return(
             <>
                 <h1 className={styles.fortune}>{this.state.fortunes}</h1>
-                <button className={styles.button}onClick={this.number}>FORTUNE</button>   
+                <button className={styles.button}onClick={this.stateDetail}>FORTUNE</button>
+                {console.log(this.state.number)}   
+                {console.log(this.state.fortunes)}   
 
             </>
         )
